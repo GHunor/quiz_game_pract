88a93c93fdd98909e068e8a40babd4811ac2079b
GHunor <45698982+GHunor@users.noreply.github.com>
2022-07-29 14:56:49 +0200

    Added score system and end message

--- quiz_game.py
+++ quiz_game.py
@@ -6,16 +6,24 @@ playing = input("Do you want to play? ")
 
 if playing.lower() != "yes":
   quit()
+  
+score = 0
 
 print("Starting now:")
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
   print("Correct")
+  score += 1
 else:
   print("Incorrect")
 
 answer = input("What does GPU stand for? ")
 if answer.lower() == "graphics processing unit":
   print("Correct")
+  score += 1
 else:
   print("Incorrect")
+
+
+print("You've got " + str(score) + " questions right!")
+print("That's " + str(score/2*100) + "%.")
