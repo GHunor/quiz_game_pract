80ae4b7270e70debafae5637b394705b2ac23f5c
GHunor <45698982+GHunor@users.noreply.github.com>
2022-08-02 13:51:19 +0200

    Main function

--- quiz_game.py
+++ quiz_game.py
@@ -1,29 +1,40 @@
 #coding: utf-8
 
-print("Welcome to my quiz!")
+#func greating
+#func load questions
+#func ask
+#func end
 
-playing = input("Do you want to play? ")
+def main():
+	print("Welcome to my quiz!")
 
-if playing.lower() != "yes":
-  quit()
+	playing = input("Do you want to play? ")
+
+	if playing.lower() != "yes":
+  		quit()
   
-score = 0
+	score = 0
 
-print("Starting now:")
-answer = input("What does CPU stand for? ")
-if answer.lower() == "central processing unit":
-  print("Correct")
-  score += 1
-else:
-  print("Incorrect")
+	print("Starting now:")
+	answer = input("What does CPU stand for? ")
+	if answer.lower() == "central processing unit":
+  		print("Correct")
+  		score += 1
+	else:
+  		print("Incorrect")
 
-answer = input("What does GPU stand for? ")
-if answer.lower() == "graphics processing unit":
-  print("Correct")
-  score += 1
-else:
-  print("Incorrect")
+	answer = input("What does GPU stand for? ")
+	if answer.lower() == "graphics processing unit":
+  		print("Correct")
+  		score += 1
+	else:
+		print("Incorrect")
 
 
-print("You've got " + str(score) + " questions right!")
-print("That's " + str(score/2*100) + "%.")
+	print("You've got " + str(score) + " questions right!")
+	print("That's " + str(score/2*100) + "%.")
+
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
