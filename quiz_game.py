d91be25f92b250c0f2519f513e268ed702cccdd5
GHunor <45698982+GHunor@users.noreply.github.com>
2022-08-05 00:34:12 +0200

    Cleaned up

--- quiz_game.py
+++ quiz_game.py
@@ -4,9 +4,42 @@
 #func load questions
 #func ask
 #func end
+def greetings(GameName):
+    print("Welcome to " + GameName +"!")
 
+    playing = input("Do you want to play? ")
+    if playing.lower() != "yes":
+        print("k thx by!")
+        quit()
+    print("Starting now:")
+    
+def ask_question(question, canswer, score):
+    answer = input(question)
+    if answer.lower() == canswer:
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+    return score
+
+def end_message(score, aoq):
+    print("You've got " + str(score) + " questions right!")
+    print("That's " + str(score/aoq*100) + "%.")
+        
 def main():
-	print("Welcome to my quiz!")
+    greetings("my quiz")
+    score = 0
+    score = ask_question("What does CPU stand for? ", "central processing unit", score)
+    score = ask_question("What does GPU stand for? ", "graphics processing unit", score)
+    end_message(score, 2)
+
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
+
+'''
+print("Welcome to my quiz!")
 
 	playing = input("Do you want to play? ")
 
@@ -33,8 +66,4 @@ def main():
 
 	print("You've got " + str(score) + " questions right!")
 	print("That's " + str(score/2*100) + "%.")
-
-# Using the special variable 
-# __name__
-if __name__=="__main__":
-    main()
+'''
